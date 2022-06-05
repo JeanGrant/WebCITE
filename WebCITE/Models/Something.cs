091c42ecf2a7b65bbccdd625fadb46105b3fcedd
JeanGrant <john_jacob_acorda@dlsl.edu.ph>
2022-06-05 12:53:14 +0800

    Add project files.

---
+++ WebCITE/Models/Something.cs
@@ -0,0 +1,11 @@
+﻿namespace WebCITE.Models
+{
+    public class Something
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Subtitle { get; set; }
+        public string RedirectURL { get; set; }
+        public string BTNText { get; set; }
+    }
+}
